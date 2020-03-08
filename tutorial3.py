f0afcfe26b438d852edcbe70ec1c2c3f8841de06
Chelsea <chelsea.br.kim@gmail.com>
2020-03-08 18:49:24 -0400

    Dictionary

---
+++ tutorial3.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+import math
+
+'''
+March 7, 2020
+
+# What is a Dictionary?
+# - written with curly braces, it is an unordered data structure (unlike a list, which is ordered)
+# - it is composed of key-value pairs; values can take on any type of data, but the keys must be "hashable" (not important rn apparently)
+# - the components are accessed by keys. In the previous assignment for example, this would allow for flexible modification of the weight categories.
+
